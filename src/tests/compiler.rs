1ea89788267efba4f9fc9f90f9b857d0bb900318
Georgy Angelov <georgyangelov@gmail.com>
2024-05-04 18:06:56 +0300

    Add some more tests for comptime export slots

--- src/tests/compiler.rs
+++ src/tests/compiler.rs
@@ -4,15 +4,21 @@ use crate::interpreter::{Interpreter, Value};
 
 macro_rules! expect {
     ($a:expr, $b:pat) => {
-        assert_match!(run($a), $b);
+        {
+            let (comptime_exports, result) = run($a);
+
+            assert_match!(result, $b);
+
+            (comptime_exports, result)
+        }
     };
 }
 
 macro_rules! assert_match {
     ($a:expr, $b:pat) => {
         {
-            let actual = $a;
-            assert!(matches!(actual, $b), "Expected {:?} to match {}", actual, stringify!($b))
+            let actual = &$a;
+            assert!(matches!(actual, &$b), "Expected {:?} to match {}", actual, stringify!($b))
         }
     };
 }
@@ -50,7 +56,7 @@ fn test_variable_assignments_in_blocks() {
 
 #[test]
 fn test_adding_numbers() {
-    expect!("41 + 1", Value::I64(42))
+    expect!("41 + 1", Value::I64(42));
 }
 
 #[test]
@@ -59,7 +65,7 @@ fn test_defining_photon_functions() {
         val fn = (a, b) a + b
 
         fn(41, 1)
-    ", Value::I64(42))
+    ", Value::I64(42));
 }
 
 #[test]
@@ -69,7 +75,7 @@ fn test_closures() {
         val fn = (b) a + b
 
         fn(1)
-    ", Value::I64(42))
+    ", Value::I64(42));
 }
 
 #[test]
@@ -79,31 +85,85 @@ fn test_closures_2() {
         val add_one = add_fn(1)
 
         add_one(41)
-    ", Value::I64(42))
+    ", Value::I64(42));
 }
 
 #[test]
 fn test_comptime_vars() {
-    expect!("
+    let (exports, _) = expect!("
         @val a = 42
 
         a
-    ", Value::I64(42))
+    ", Value::I64(42));
+
+    assert_match!(&exports[0], &Value::I64(42));
+}
+
+#[test]
+fn test_comptime_vars_in_fn_scopes() {
+    expect!("
+        val a = {
+            @val a = 40 + 1
+
+            { a + 1 }
+        }
+
+        a()()
+    ", Value::I64(42));
+}
+
+#[test]
+fn test_comptime_vars_in_comptime_fns() {
+    expect!("
+        @val a = {
+            @val a = 42
+
+            a
+        }
+
+        a()
+    ", Value::I64(42));
+}
+
+#[test]
+fn test_capture_of_comptime_vars_in_comptime_fns() {
+    expect!("
+        @val a = 41
+        @val fn = {
+            a + 1
+        }
+
+        fn()
+    ", Value::I64(42));
+}
+
+#[test]
+fn test_comptime_expressions() {
+    let (exports, _) = expect!("
+        @val a = 39
+
+        1 + @(a + 1) + 1
+    ", Value::I64(42));
+
+    assert_match!(&exports[0], &Value::I64(40));
 }
 
 // fn expect(code: &str, expected: Value) {
 //     expect!(run(code), expected)
 // }
 
-fn run(code: &str) -> Value {
+fn run(code: &str) -> (Vec<Value>, Value) {
     let ast = parse(code).expect("Could not parse");
     let module = ModuleCompiler::compile_module(ast).expect("Could not compile");
 
     let mut interpreter = Interpreter::new();
 
     let comptime_result = interpreter.eval_module_comptime(&module);
+    let exports = comptime_result.comptime_exports.clone();
 
-    interpreter.eval_module_runtime(&module, comptime_result.comptime_exports)
+    let result = interpreter.eval_module_runtime(&module, comptime_result.comptime_exports);
+
+    (exports, result)
 }
 
 fn parse(code: &str) -> Result<AST, ParseError> {
